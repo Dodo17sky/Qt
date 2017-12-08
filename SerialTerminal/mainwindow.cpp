84e39526e1389c730592b8c02ea022addeaf5635
Dodo <dodoyln@hotmail.com>
2017-12-08 16:43:21 +0200

    SerialTerminal: check for available ports

--- SerialTerminal/mainwindow.cpp
+++ SerialTerminal/mainwindow.cpp
@@ -1,14 +1,29 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QtSerialPort/QSerialPortInfo>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    m_cboxPortList = ui->cboxPortList;
+
+    checkAvailablePorts();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::checkAvailablePorts()
+{
+    m_cboxPortList->addItem(" ");
+
+    Q_FOREACH(QSerialPortInfo port, QSerialPortInfo::availablePorts())
+    {
+        m_cboxPortList->addItem(port.portName());
+    }
 }
--- SerialTerminal/mainwindow.h
+++ SerialTerminal/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtWidgets/QComboBox>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private: // functions
+    void checkAvailablePorts(void);
+
 private:
     Ui::MainWindow *ui;
+
+    QComboBox *m_cboxPortList;
 };
 
 #endif // MAINWINDOW_H
